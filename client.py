21cdcb35a3f6b9e4c91ec15b6343c2b41c1322ee
Yuri Kilochek <yuri.kilochek@gmail.com>
2014-10-04 18:30:14 +0400

    Implemented server selection.

--- client.py
+++ client.py
@@ -4,6 +4,7 @@ from PyQt4.QtGui import *
 from PyQt4.QtNetwork import *
 
 import config
+from ServerList import *
 
 
 class Client(QApplication):
@@ -14,21 +15,21 @@ class Client(QApplication):
         self.announce_receiver.bind(QHostAddress.Any, config.announce_port)
         self.announce_receiver.readyRead.connect(self.read_announce_receiver)
 
-        # self.main_window = QMainWindow()
-        # self.main_window.resize(250, 150)
-        # self.main_window.move(300, 300)
-        # self.main_window.setWindowTitle('Sample Text')
-        # self.main_window.show()
+        self.server_list = ServerList()
+        self.server_list.on_pick_server = self.on_pick_server
+        self.server_list.show()
 
     def read_announce_receiver(self):
-        print('read_announce_receiver')
         while self.announce_receiver.hasPendingDatagrams():
             message, address, _ = self.announce_receiver.readDatagram(self.announce_receiver.pendingDatagramSize())
             if message.decode('UTF-8') == config.announce_message:
                 self.on_announce(address.toString())
 
     def on_announce(self, server_address):
-        print('Server {} announced itself'.format(server_address))
+        self.server_list.add_server(server_address)
+
+    def on_pick_server(self, server_address):
+        print('Server {} picked'.format(server_address))
 
 if __name__ == '__main__':
     sys.exit(Client(sys.argv).exec_())
