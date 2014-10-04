c497f7c2a16ce669cdc2c6177126eb45f5e178dc
Yuri Kilochek <yuri.kilochek@gmail.com>
2014-10-04 17:02:41 +0400

    Servers can now announce themselves to clients.

--- client.py
+++ client.py
@@ -1,21 +1,34 @@
 import sys
 
 from PyQt4.QtGui import *
+from PyQt4.QtNetwork import *
+
+import config
 
 
 class Client(QApplication):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.main_window = QMainWindow()
-        self.main_window.resize(250, 150)
-        self.main_window.move(300, 300)
-        self.main_window.setWindowTitle('Sample Text')
+        self.announce_receiver = QUdpSocket()
+        self.announce_receiver.bind(QHostAddress.Any, config.announce_port)
+        self.announce_receiver.readyRead.connect(self.read_announce_receiver)
 
-    def exec_(self):
-        self.main_window.show()
+        # self.main_window = QMainWindow()
+        # self.main_window.resize(250, 150)
+        # self.main_window.move(300, 300)
+        # self.main_window.setWindowTitle('Sample Text')
+        # self.main_window.show()
 
-        return super().exec_()
+    def read_announce_receiver(self):
+        print('read_announce_receiver')
+        while self.announce_receiver.hasPendingDatagrams():
+            message, address, _ = self.announce_receiver.readDatagram(self.announce_receiver.pendingDatagramSize())
+            if message.decode('UTF-8') == config.announce_message:
+                self.on_announce(address.toString())
+
+    def on_announce(self, server_address):
+        print('Server {} announced itself'.format(server_address))
 
 if __name__ == '__main__':
     sys.exit(Client(sys.argv).exec_())
--- server.py
+++ server.py
@@ -10,20 +10,16 @@ class Server(QCoreApplication):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.announce_socket = QUdpSocket()
+        self.announce_emitter = QUdpSocket()
 
-        self.announce_timer = QTimer()
-        self.announce_timer.setInterval(int(config.announce_interval * 1000))
-        self.announce_timer.timeout.connect(self.announce)
+        self.announce_repeater = QTimer()
+        self.announce_repeater.timeout.connect(self.announce)
+        self.announce_repeater.start(int(config.announce_interval * 1000))
 
     def announce(self):
         print('announce')
-        self.announce_socket.writeDatagram(config.announce_message, QHostAddress.Broadcast, config.announce_port)
+        self.announce_emitter.writeDatagram(config.announce_message, QHostAddress.Broadcast, config.announce_port)
 
-    def exec_(self):
-        self.announce_timer.start()
-
-        return super().exec_()
 
 if __name__ == '__main__':
     sys.exit(Server(sys.argv).exec_())
