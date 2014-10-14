b5eb15ce9e8b509c22d9013051f6e1b5693deda0
Yuri Kilochek <yuri.kilochek@gmail.com>
2014-10-14 17:30:29 +0400

    Fixed minor naming mishap.

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import sys as _sys
 
 import PyQt4.QtCore as _QtCore
 
-from server_lookup import ServerAnnouncer as _ServerAnnounce
+from server_lookup import ServerAnnouncer as _ServerAnnouncer
 from connection import Connection as _Connection
 
 
@@ -10,7 +10,7 @@ class _Server(_QtCore.QCoreApplication):
     def __init__(self, argv):
         super().__init__(argv)
 
-        self._announcer = _ServerAnnounce()
+        self._announcer = _ServerAnnouncer()
 
         self._client_awaiter = _Connection.Awaiter()
         self._client_awaiter.connected.connect(self._on_client_connected)
