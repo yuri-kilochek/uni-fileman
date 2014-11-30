6402db93c4ff82854b0b51c1a8bce9f8d94164c2
yuri_kilochek <yuri.kilochek@gmail.com>
2014-11-30 18:00:58 +0300

    Fixed yet more comments.

--- config.py
+++ config.py
@@ -2,15 +2,15 @@ import io as _io
 import json as _json
 
 config = {
-    'announcement-token': 'fileman',  	# токен, который сервер рассылает клиентам
-    'announcement-port': 27072,  		# порт, через который сервер обьявляет о себе
-    'connection-port': 27073,  			# порт подключения
-    'connection-timeout': 3.0,  		# максимальное время ожидания установки соединения
-    'heartbeat-interval': 0.5,  		# интервал проверки целостности соединения
-    'root-directory': '',  				# корневая директория
-    'log-file': 'log.txt', 		 		# файл журнала
-    'users-database': 'users.json',  	# база данных пользователей
-    'gui-language': 'en_US',  			# язык пользовательского интерфейса
+    'announcement-token': 'fileman',    # токен, который сервер рассылает клиентам
+    'announcement-port': 27072,         # порт, через который сервер обьявляет о себе
+    'connection-port': 27073,           # порт подключения
+    'connection-timeout': 3.0,          # максимальное время ожидания установки соединения
+    'heartbeat-interval': 0.5,          # интервал проверки целостности соединения
+    'root-directory': '',               # корневая директория
+    'log-file': 'log.txt',              # файл журнала
+    'users-database': 'users.json',     # база данных пользователей
+    'gui-language': 'en_US',            # язык пользовательского интерфейса
 }
 
 with _io.open('config.json', mode='r', encoding='UTF-8') as config_file:
